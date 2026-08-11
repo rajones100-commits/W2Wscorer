48764df73b9bf293111f2282b6f645bbdb7a6288
rajones100-commits <rajones100@gmail.com>
2026-08-11 20:05:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-// Copy your Supabase Project URL and publishable/anon key here after running database.sql.
+NEXT_PUBLIC_SUPABASE_URL=https://imskmspmlzanghdbqkwd.supabase.co
+NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_JdMiVeYABtg0QjDjB9PeYA_u1yg-z47
 // The scorer still works locally when these are left blank.
 window.W2W_DB_CONFIG = {
   supabaseUrl: '',
