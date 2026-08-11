9a1e92969a3984310730912769272be607d7b09f
rajones100-commits <rajones100@gmail.com>
2026-08-11 20:51:14 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-NEXT_PUBLIC_SUPABASE_URL=https://imskmspmlzanghdbqkwd.supabase.co
-NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_JdMiVeYABtg0QjDjB9PeYA_u1yg-z47
-// The scorer still works locally when these are left blank.
-window.W2W_DB_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
-};
